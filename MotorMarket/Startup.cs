879e461896b76e57e83d6b75c362f185f237ba03
mtengineer90 <murattaksimm@gmail.com>
2020-12-29 00:18:14 +0300

    Ayar ve Konfigürasyonlar

--- MotorMarket/Startup.cs
+++ MotorMarket/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MotorMarket.AppDbContext;
 
 namespace MotorMarket
 {
@@ -24,7 +26,10 @@ namespace MotorMarket
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MgaleriDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
