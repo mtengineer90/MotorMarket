46dd7b2dee87ec37dedba4bb4b761958e506e913
mtengineer90 <murattaksimm@gmail.com>
2020-12-28 23:54:52 +0300

    Proje Oluşturulması

---
+++ MotorMarket/AppDbContext/MgaleriDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotorMarket.AppDbContext
+{
+    public class MgaleriDbContext:DbContext
+    {
+        public MgaleriDbContext(DbContextOptions<MgaleriDbContext> options):base(options)
+        {
+
+        }
+    }
+}
