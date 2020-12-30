f91a52279f7488328981a9c263e5e12c312253b9
mtengineer90 <murattaksimm@gmail.com>
2020-12-31 00:17:58 +0300

    Controllers...

---
+++ MotorMarket/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MotorMarket.Models;
+
+namespace MotorMarket.Controllers
+{
+    public class MainController : Controller
+    {
+        //main/motors
+        public IActionResult Motors()
+        {
+            Main main = new Main { Id = 1, Name = "Harley Davidson" };
+
+            return View(main);
+        }
+    }
+}
