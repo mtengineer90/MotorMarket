da94046cb9b0d16f992847ee0d1aa919faf95c07
mtengineer90 <murattaksimm@gmail.com>
2021-01-02 00:28:34 +0300

    Sayfa&Tablo Oluşturulması...

--- MotorMarket/Controllers/MainController.cs
+++ MotorMarket/Controllers/MainController.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MotorMarket.AppDbContext;
 using MotorMarket.Models;
 
 namespace MotorMarket.Controllers
 {
     public class MainController : Controller
     {
-        //main/motors
-        [Route("main")]
-        [Route("main/motors")]
-        public IActionResult Motors()
-        {
-            Main main = new Main { Id = 1, Name = "Harley Davidson" };
+        private readonly MgaleriDbContext _db;
 
-            return View(main);
+        public MainController(MgaleriDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Mains.ToList());
         }
 
-        [Route("main/motors/{year:int:length(4)}/{month:int:range(1,12)}")]
-        public IActionResult ByYearMonth(int year, int month)
-        {
-            return Content(year + ";" + month);
-        }
+        ////main/motors
+        //[Route("main")]
+        //[Route("main/motors")]
+        //public IActionResult Motors()
+        //{
+        //    Main main = new Main { Id = 1, Name = "Harley Davidson" };
+
+        //    return View(main);
+        //}
+
+        //[Route("main/motors/{year:int:length(4)}/{month:int:range(1,12)}")]
+        //public IActionResult ByYearMonth(int year, int month)
+        //{
+        //    return Content(year + ";" + month);
+        //}
+   
     }
 }
