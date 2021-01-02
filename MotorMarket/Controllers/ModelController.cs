5e8273e7840a118564acc8331e6c1d1ad8269413
mtengineer90 <murattaksimm@gmail.com>
2021-01-02 20:32:15 +0300

    Motorsiklet Modellerin Oluşturulması...

---
+++ MotorMarket/Controllers/ModelController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MotorMarket.AppDbContext;
+using MotorMarket.Models;
+using MotorMarket.Models.ViewModels;
+
+namespace MotorMarket.Controllers
+{
+    public class ModelController : Controller
+    {
+        private readonly MgaleriDbContext _db;
+        [BindProperty]
+        public ModelViewModel ModelVM { get; set; }
+        public ModelController(MgaleriDbContext db)
+        {
+            _db = db;
+            ModelVM = new ModelViewModel()
+            {
+                Mains = _db.Mains.ToList(),
+                Model = new Models.Model()
+            };
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Models.Include(x => x.Main);
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View(ModelVM);
+        }
+        [HttpPost, ActionName("Create")]
+        public IActionResult CreatePost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View (ModelVM);
+            }
+            _db.Models.Add(ModelVM.Model);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MotorMarket/Models/ViewModels/ModelViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotorMarket.Models.ViewModels
+{
+    public class ModelViewModel
+    {
+        public Model Model { get; set; }
+        public IEnumerable<Main> Mains { get; set; }
+        public IEnumerable<SelectListItem> CSelectListItem(IEnumerable<Main> Items)
+        {
+            List<SelectListItem> MainList = new List<SelectListItem>();
+            SelectListItem sli = new SelectListItem
+            {
+                Text = "---Seç---",
+                Value = "0"
+            };
+            MainList.Add(sli);
+            foreach (Main main in Items)
+            {
+                sli = new SelectListItem
+                {
+                    Text = main.Name,
+                    Value = main.Id.ToString()
+                };
+                MainList.Add(sli);
+            }
+            return MainList;
+
+        }
+    }
+}
