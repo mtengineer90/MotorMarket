4f5e25e536880d1841e6199b4f662c1e7f0f270b
mtengineer90 <murattaksimm@gmail.com>
2021-01-02 23:33:11 +0300

    Motorsiklet Modelleri Düzenleme-Silme Özellikleri ve Extensions...

---
+++ MotorMarket/Extensions/ExtensionSinif.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotorMarket.Extensions
+{
+    public static class ExtensionSinif
+    {
+        public static string GetPropertyValue<T>(this T item, string propertyName)
+        {
+            return item.GetType().GetProperty(propertyName).GetValue(item, null).ToString();
+        }
+    }
+}
---
+++ MotorMarket/Extensions/IENumerableExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MotorMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotorMarket.Extensions
+{
+    public static class IENumerableExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> Items)
+        {
+            List<SelectListItem> List = new List<SelectListItem>();
+            SelectListItem sli = new SelectListItem
+            {
+                Text = "-Seçiniz-",
+                Value = "0"
+            };
+            List.Add(sli);
+            foreach (var item in Items)
+            {
+                sli = new SelectListItem
+                {
+                    Text = item.GetPropertyValue("Name"),
+                    Value = item.GetPropertyValue("Id")
+                   // Text = item.GetType().GetProperty("Name").GetValue(item, null).ToString(),
+                   // Value = item.GetType().GetProperty("Id").GetValue(item, null).ToString(),
+                };
+                List.Add(sli);
+            }
+            return List;
+
+        }
+    }
+}
